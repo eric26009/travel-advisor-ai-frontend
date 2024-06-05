27dab0961f50a2043a916aa28b1b2001a2e95339
eric26009 <ericfeldman@outlook.com>
2024-06-05 16:40:08 -0700

    Steve/trip advisor page (#1)
    * more input boxes, responsive, extract ai response
    
    * adds loading spinner when request is pending
    
    * adds global style for button and tabs, removes footer and header, makes month select and trip type a  dropdown selection type
    
    * adds animation svg airplane for loading
    
    * makes us of ids as keys in list generation
    
    ---------
    
    Co-authored-by: Steve Chen <chensteve520@gmail.com>
    Co-authored-by: Eric Feldman <eric.feldman@rotellacapital.com>

---
+++ tomatoegg/src/app/components/spinner/Spinner.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+interface Props {
+  height?: number;
+  width?: number;
+}
+
+// Source code for spinner
+// https://github.com/n3r4zzurr0/svg-spinners/blob/main/svg-smil/ring-resize.svg?short_path=11c2641
+
+const Spinner = ({ width, height }: Props) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      stroke="#000"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g>
+        <circle
+          cx="12"
+          cy="12"
+          r="9.5"
+          fill="none"
+          stroke-width="3"
+          stroke-linecap="round"
+        >
+          <animate
+            attributeName="stroke-dasharray"
+            dur="1.5s"
+            calcMode="spline"
+            values="0 150;42 150;42 150;42 150"
+            keyTimes="0;0.475;0.95;1"
+            keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
+            repeatCount="indefinite"
+          />
+          <animate
+            attributeName="stroke-dashoffset"
+            dur="1.5s"
+            calcMode="spline"
+            values="0;-16;-59;-59"
+            keyTimes="0;0.475;0.95;1"
+            keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
+            repeatCount="indefinite"
+          />
+        </circle>
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          dur="2s"
+          values="0 12 12;360 12 12"
+          repeatCount="indefinite"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export { Spinner };
--- tomatoegg/src/app/components/travelPlanner.tsx
+++ tomatoegg/src/app/components/travelPlanner.tsx
@@ -1,27 +1,55 @@
 "use client";
 import axios from "axios";
 import React, { useState } from "react";
+import { Spinner } from "./spinner/Spinner";
+import AirplaneImage from "./airplaneImage";
+import "./index.css";
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const tripTypes = [
+  { label: "Road Trip", value: "roadtrip" },
+  { label: "Domestic", value: "domestic" },
+  { label: "International", value: "international" },
+];
 
 const TravelPlanner = () => {
   const [selectKnownLocation, setSelectKnownLocation] = useState(true);
   const [destination, setDestination] = useState("");
   const [startLocation, setStartLocation] = useState("");
-  const [travelType, setTravelType] = useState("");
-  const [month, setMonth] = useState("");
+  const [travelType, setTravelType] = useState("roadtrip");
+  const [month, setMonth] = useState("January");
   const [accessCode, setAccessCode] = useState("");
   const [error, setError] = useState("");
-  const [response, setResponse] = useState(null);
+  const [activities, setActivities] = useState([]);
+  const [destinations, setDestinations] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const handleLocationClick = (value: boolean) => {
     setSelectKnownLocation(value);
-    setDestination("");
-    setStartLocation("");
-    setTravelType("");
-    setMonth("");
+    // setDestination("");
+    // setStartLocation("");
+    // setTravelType("");
+    // setMonth("");
   };
 
   const submit = () => {
     setError("");
+    setActivities([]);
+    setDestinations([]);
     if (selectKnownLocation) {
       if (!destination || !month) {
         setError("Missing parameters");
@@ -30,6 +58,7 @@ const TravelPlanner = () => {
       }
     } else {
       if (!startLocation || !travelType || !month) {
+        setError("Missing parameters");
       } else {
         fetchUnknownLocation();
       }
@@ -38,11 +67,15 @@ const TravelPlanner = () => {
 
   const fetchKnownLocation = async () => {
     try {
-      const response = await axios.get(
-        `http://localhost:8000/travel/known?endLocation=${destination}&month=${month}`,
-        { headers: { Authorization: accessCode } }
-      );
-      setResponse(response.data);
+      setLoading(true);
+      const response = await axios
+        .get(
+          `http://localhost:8000/travel/known?endLocation=${destination}&month=${month}`,
+          { headers: { Authorization: accessCode } }
+        )
+        .finally(() => setLoading(false));
+      console.log(response);
+      setActivities(response.data.activities);
     } catch (err: any) {
       console.log(err);
       if (err?.message) {
@@ -56,12 +89,15 @@ const TravelPlanner = () => {
 
   const fetchUnknownLocation = async () => {
     try {
-      const response = await axios.get(
-        `http://localhost:8000/travel/unknown?startLocation=${startLocation}&type=${travelType}&month=${month}`,
-        { headers: { Authorization: accessCode } }
-      );
+      setLoading(true);
+      const response = await axios
+        .get(
+          `http://localhost:8000/travel/unknown?startLocation=${startLocation}&type=${travelType}&month=${month}`,
+          { headers: { Authorization: accessCode } }
+        )
+        .finally(() => setLoading(false));
 
-      setResponse(response.data);
+      setDestinations(response.data.destinations);
     } catch (err: any) {
       console.log(err);
       if (err?.message) {
@@ -78,16 +114,12 @@ const TravelPlanner = () => {
       <span className="flex justify-center text-3xl font-bold pb-5">
         Travel Advisor AI
       </span>
-      <main className="flex flex-col items-center justify-center">
-        <div className="w-full max-w-lg">
+      <main className="items-center justify-center">
+        <div className="flex flex-col w-full px-8">
           <div className="flex border-b">
             <div className="-mb-px mr-1 w-full">
               <button
-                className={`bg-white inline-block py-2 px-4 w-full ${
-                  selectKnownLocation
-                    ? "border-l border-t border-r rounded-t"
-                    : ""
-                }`}
+                className={selectKnownLocation ? "tab-active" : "tab-inactive"}
                 onClick={() => handleLocationClick(true)}
               >
                 Known Location
@@ -95,120 +127,177 @@ const TravelPlanner = () => {
             </div>
             <div className="-mb-px w-full">
               <button
-                className={`bg-white inline-block py-2 px-4 w-full ${
-                  selectKnownLocation
-                    ? ""
-                    : "border-l border-t border-r rounded-t"
-                }`}
+                className={selectKnownLocation ? "tab-inactive" : "tab-active"}
                 onClick={() => handleLocationClick(false)}
               >
                 Unknown Location
               </button>
             </div>
           </div>
-          {selectKnownLocation ? (
-            <>
-              <div className="flex border-x justify-center items-center pt-5">
-                <div className="w-[100px]">Destination:</div>
-                <input
-                  className="w-[200px] mx-5 py-1 px-3 border rounded focus:outline-none"
-                  type="text"
-                  placeholder="Seattle"
-                  value={destination}
-                  onChange={(e) => setDestination(e.target.value)}
-                />
-              </div>
-              <div className="flex border-x justify-center items-center py-5">
-                <div className="w-[100px]">Month:</div>
-                <input
-                  className="w-[200px] mx-5 py-1 px-3 border rounded focus:outline-none"
-                  type="text"
-                  placeholder="May"
-                  value={month}
-                  onChange={(e) => setMonth(e.target.value)}
-                />
-              </div>
-            </>
-          ) : (
-            <>
-              <div className="flex border-x justify-center items-center pt-5">
-                <div className="w-[150px]">Start Location:</div>
-                <input
-                  className="w-[200px]  py-1 px-3 border rounded focus:outline-none"
-                  type="text"
-                  placeholder="Seattle"
-                  value={startLocation}
-                  onChange={(e) => setStartLocation(e.target.value)}
-                />
-              </div>
-              <div className="flex border-x justify-center items-center pt-5">
-                <label className="flex p-3">
+          <div className="flex flex-wrap border-x border-black">
+            {selectKnownLocation ? (
+              <>
+                <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 flex justify-center items-center pt-5 bg-gray-100">
+                  <div className="w-[150px] bg-gray-100">Destination:</div>
                   <input
-                    type="radio"
-                    name="travelType"
-                    value="domestic"
-                    checked={travelType == "domestic"}
-                    onChange={(e) => setTravelType(e.target.value)}
+                    className="w-[150px] py-1 px-3 border rounded focus:outline-none bg-white border-black"
+                    type="text"
+                    placeholder="Seattle"
+                    value={destination}
+                    onChange={(e) => setDestination(e.target.value)}
                   />
-                  <span className="ml-3">Domestic</span>
-                </label>
-                <label className="flex p-3">
+                </div>
+                <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 flex justify-center items-center pt-5 bg-gray-100">
+                  <div className="w-[150px] bg-gray-100">Budget:</div>
                   <input
-                    type="radio"
-                    name="travelType"
-                    value="roadtrip"
-                    checked={travelType == "roadtrip"}
-                    onChange={(e) => setTravelType(e.target.value)}
+                    className="w-[150px] py-1 px-3 border rounded focus:outline-none bg-white border-black"
+                    type="text"
+                    placeholder="$$"
+                    //value={}
+                    //onChange={}
                   />
-                  <span className="ml-3">Road Trip</span>
-                </label>
-                <label className="flex p-3">
+                </div>
+                <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 flex justify-center items-center pt-5 bg-gray-100">
+                  <div className="w-[150px] bg-gray-100">Month:</div>
+                  <select
+                    className="w-[150px] py-1 px-3 border rounded focus:outline-none bg-white border-black"
+                    value={month}
+                    onChange={(e) => setMonth(e.target.value)}
+                  >
+                    {months.map((m) => (
+                      <option key={m} value={m}>
+                        {m}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 flex justify-center items-center pt-5 bg-gray-100">
+                  <div className="w-[150px] bg-gray-100">Travelers:</div>
                   <input
-                    type="radio"
-                    name="travelType"
-                    value="international"
-                    checked={travelType == "international"}
-                    onChange={(e) => setTravelType(e.target.value)}
+                    className="w-[150px] py-1 px-3 border rounded focus:outline-none bg-white border-black"
+                    type="text"
+                    placeholder="2"
+                    //value={}
+                    //onChange={}
                   />
-                  <span className="ml-3">International</span>
-                </label>
-              </div>
-              <div className="flex border-x justify-center items-center py-5">
-                <div className="w-[100px]">Month:</div>
-                <input
-                  className="w-[200px] mx-5 py-1 px-3 border rounded focus:outline-none"
-                  type="text"
-                  placeholder="May"
-                  value={month}
-                  onChange={(e) => setMonth(e.target.value)}
-                />
-              </div>
-            </>
-          )}
-          <div className="flex border-x justify-center items-end pb-5 border-b">
-            <div className="flex-column">
-              <div className="w-[200px]">Access Code:</div>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 flex justify-center items-center pt-5 bg-gray-100">
+                  <div className="w-[150px] bg-gray-100">Start Location:</div>
+                  <input
+                    className="w-[150px] py-1 px-3 border rounded focus:outline-none bg-white border-black"
+                    type="text"
+                    placeholder="Seattle"
+                    value={startLocation}
+                    onChange={(e) => setStartLocation(e.target.value)}
+                  />
+                </div>
+                <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 flex justify-center items-center pt-5 bg-gray-100">
+                  <div className="w-[150px] bg-gray-100">Month:</div>
+                  <select
+                    className="w-[150px] py-1 px-3 border rounded focus:outline-none bg-white border-black"
+                    value={month}
+                    onChange={(e) => setMonth(e.target.value)}
+                  >
+                    {months.map((m) => (
+                      <option key={m} value={m}>
+                        {m}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 flex justify-center items-center pt-5 bg-gray-100">
+                  <div className="w-[150px] bg-gray-100">Trip Type:</div>
+                  <select
+                    className="w-[150px] py-1 px-3 border rounded focus:outline-none bg-white border-black"
+                    value={travelType}
+                    onChange={(e) => setTravelType(e.target.value)}
+                  >
+                    {tripTypes.map((type) => (
+                      <option key={type.value} value={type.value}>
+                        {type.label}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </>
+            )}
+          </div>
+          <div className="flex flex-wrap border-x border-b rounded-b border-black pb-5 bg-gray-100">
+            <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 flex justify-center items-center pt-5 bg-gray-100">
+              <div className="w-[150px] bg-gray-100">Access Code:</div>
               <input
-                className="w-[200px] mr-2 py-1  border rounded focus:outline-none"
-                type="text"
-                placeholder=""
+                className="w-[150px] py-1 px-3 border rounded focus:outline-none bg-white border-black"
+                type="password"
+                placeholder="Code"
                 value={accessCode}
                 onChange={(e) => setAccessCode(e.target.value)}
               />
             </div>
-            <button
-              className="w-[50%] py-2 px-4 rounded bg-black text-white font-bold border border-transparent hover:bg-white hover:text-black hover:border-black"
-              type="button"
-              onClick={() => submit()}
-            >
-              Submit
-            </button>
+            <div className="w-full md:w-1/2  lg:w-1/2 xl:w-1/2 flex justify-center items-center pt-5 bg-gray-100">
+              <button
+                className="btn-primary"
+                type="button"
+                onClick={() => submit()}
+                disabled={loading}
+              >
+                Submit
+              </button>
+              {loading && <Spinner height={40} width={40} />}
+            </div>
+            {error && <span className="text-red-500">{error}</span>}
           </div>
-          {error && <span className="text-red-500">{error}</span>}
         </div>
-        {response && (
+        {loading && (
+          <div className="mx-auto w-3/12 h-3/12 ">
+            <AirplaneImage />
+          </div>
+        )}
+        {activities && (
           <div className="w-full">
-            <pre>{JSON.stringify(response, null, 2)}</pre>
+            {/*<pre>{JSON.stringify(response, null, 2)}</pre>*/}
+            {activities.map((activitie: any) => {
+              return (
+                <div
+                  className="flex flex-col border border-black rounded mx-8 mt-5 p-3 bg-gray-300"
+                  key={activitie.id}
+                >
+                  <div className="bg-gray-300 mt-1 flex w-full justify-between">
+                    <div className="bg-gray-300 font-bold">
+                      {activitie.title}
+                    </div>
+                    <div className="bg-gray-300">{activitie.type}</div>
+                  </div>
+                  <div className="bg-gray-300 mt-1">{activitie.address}</div>
+                  <div className="bg-gray-300 mt-5">
+                    {activitie.description}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
+        {destinations && (
+          <div className="w-full">
+            {destinations.map((destination: any) => {
+              return (
+                <div
+                  className="flex flex-col border border-black rounded mx-8 mt-5 p-3 bg-gray-300"
+                  key={destination.id}
+                >
+                  <div className="bg-gray-300 mt-1 flex w-full">
+                    <div className="bg-gray-300 font-bold">
+                      {destination.location}
+                    </div>
+                  </div>
+                  <div className="bg-gray-300 mt-3">
+                    {destination.description}
+                  </div>
+                </div>
+              );
+            })}
           </div>
         )}
       </main>
